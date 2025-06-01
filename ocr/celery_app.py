dd3cd3adc3c5ff7e63ae23a4f50caf2fcd2f934f
thamerkt <kthirithamer1@gmail.com>
2025-06-01 20:18:46 +0100

    Update celery_app.py

--- ocr/celery_app.py
+++ ocr/celery_app.py
@@ -1,6 +1,6 @@
 from celery import Celery
 
-app = Celery('scraper_app', broker='redis://host.docker.internal:6379/0')
+app = Celery('scraper_app', broker='redis://red-d0uaek2dbo4c73apegag:6379')
 
 # Optional: If you want to schedule tasks
 app.conf.beat_schedule = {
